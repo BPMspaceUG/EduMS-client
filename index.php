bc63d419c28451ff8d91089b194da0c0d7f70ef6
cnu301 <->
2016-02-24 20:38:53 +0100

    review - clean - comment

--- index.php
+++ index.php
@@ -1,51 +1,54 @@
 <?php
 
+/*Define self and location*/
 $config['srv']['addr'] = 'http://localhost:4040/EduMS/api/index.php';
 $config['auth']['login'] = 'BPMspace';
 $config['auth']['token'] = 'abc';
 $baseURL = $config['srv']['addr'].'/'.$config['auth']['login'].'/'.$config['auth']['token'];
 
-if(array_key_exists('contentid',$_GET)){
-    intval($_GET['contentid'])>0 ? $contentid="/".intval($_GET['contentid']) : $contentid="";
-}
-
-$navigationDestiny = '';
+/*Handle the requested view and define the URL*/
+$responseurl = $baseURL."/";
 if(array_key_exists('navdest',$_GET)){
     $navigationDestiny = $_GET['navdest'];
+    switch ($_GET['navdest']){
+        case 'brand': $responseurl = $baseURL.'/brand';
+            break;
+        case 'monitor': $responseurl = $baseURL.'/monitor';
+            break;
+        default:  $responseurl = $baseURL.'/';
+    }
 }
 
-$responseurl = "";
-switch ($navigationDestiny){
-    case 'brand': $responseurl = $baseURL.'/brand';
-        break;
-    case 'monitor': $responseurl = $baseURL.'/monitor';
-        break;
-    default:  $responseurl = $baseURL.'/';
-}
-
+/*Fetch the data from the api*/
 $response = file_get_contents($responseurl);
 
-
-if(isset($_REQUEST['debug']) && $_REQUEST['debug']==18234){
-    echo "<hr>".($responseurl)."<hr>";
+/*To echo out a debuging version of the response, add "&debug=1" to the URL.*/
+if(isset($_REQUEST['debug']) && $_REQUEST['debug']==1){
+    echo "<hr> The response-URL:'".($responseurl)."' responses as Array:<hr>";
     var_dump(is_array(json_decode($response, true)));
-    echo "<hr>";
+    echo "<hr> And just JSON-decoded";
     var_dump(json_decode($response, true));
     echo "<hr>";
 }
+
+/*The response is expected as a valid JSON-Array. If not echo the fail.*/
 if(!is_array(json_decode($response, true))){
-    echo "Invalid response from Server - could not parse data";
-    echo "<h2>Data is:</h2>";
-    echo json_decode($response);
-    echo "<h3>RAW:</h3>";
+    echo "<h2>The response is not a valid JSON-Array</h2>";
+    echo "<h3>$response:</h3>";
     echo $response;
+    echo "<hr>";
     exit;
 }
- 
+
+/*If the authentification on the server was successfull, seperate the valid $response*/ 
 $response = json_decode($response, true);
 if (is_array($response)) {
     if ($response != "invalidCredentials") {
-        $ct = $response['ct']; $script = $response['script']; $controller = $response['controller']; $css = $response['css']; $directive = $response['directive'];
+        if(array_key_exists('ct',$response)){
+            $ct = $response['ct']; $script = $response['script']; $controller = $response['controller']; $css = $response['css']; $directive = $response['directive'];
+        }else{
+            $ct = 'no content'; $script = ''; $controller = ''; $css = ''; $directive = '';
+        }
     }
     else{
         $ct = 'invalid c'; $script = ''; $controller = ''; $css = ''; $directive = '';
@@ -55,7 +58,7 @@ else{
     $ct = 'Die empfangenen Daten sind kein Array.'; $script = ''; $controller = ''; $css = ''; $directive = '';
 }
 
-//Konstruktion der Ausgabepage
+/*Create the page.*/
 echo <<<EOF
 <!DOCTYPE html>
 <html lang="de" ng-app='application'>
